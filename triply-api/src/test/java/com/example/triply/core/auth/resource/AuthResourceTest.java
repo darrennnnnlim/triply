78eef49618d8a18f4770d5a8ec54502eb04e8e8a
Darren Lim <darrenlimqinyi@hotmail.com>
2025-01-29 12:40:38 +0800

    added test case

---
+++ triply-api/src/test/java/com/example/triply/core/auth/resource/AuthResourceTest.java
@@ -0,0 +1,25 @@
+package com.example.triply.core.auth.resource;
+
+import com.example.triply.TriplyApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = TriplyApplication.class)
+@WebMvcTest(AuthResource.class)
+public class AuthResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void loginTest() throws Exception {
+        mockMvc.perform(get("/api/${triply.api-version}/auth")).andExpect(status().isOk()).andExpect(content().string("Done"));
+    }
+}
