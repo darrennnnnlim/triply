4d8abbc1665d273b8064e9d5419e544221b0780c
Darren Lim <darrenlimqinyi@hotmail.com>
2025-03-25 21:26:44 +0800

    removed redundant comments

--- triply-app/src/app/auth/login/login.component.ts
+++ triply-app/src/app/auth/login/login.component.ts
@@ -1,58 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { AuthService } from '../auth.service';
-// import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css'],
-//   standalone: false,
-// })
-// export class LoginComponent {
-//   loginForm: FormGroup;
-//   errorMessage: string = '';
-//   isLoading: boolean = false;
-
-//   constructor(
-//     private fb: FormBuilder,
-//     private authService: AuthService,
-//     private router: Router,
-//     public activeModal: NgbActiveModal
-//   ) {
-//     this.loginForm = this.fb.group({
-//       username: ['', Validators.required],
-//       password: ['', Validators.required],
-//       role: ['USER', Validators.required],
-//     });
-//   }
-
-//   onSubmit(): void {
-//     if (this.loginForm.invalid) {
-//       return;
-//     }
-//     this.isLoading = true;
-//     this.errorMessage = '';
-
-//     const { username, password, role } = this.loginForm.value;
-//     this.authService.login({ username, password, role }).subscribe({
-//       next: () => {
-//         this.activeModal.close('Login successful');
-//         this.router.navigate(['/']);
-//         this.isLoading = false;
-//       },
-//       error: (err) => {
-//         console.log(err);
-//         this.errorMessage =
-//           err.error?.message || 'Invalid username or password';
-//         this.isLoading = false;
-//       },
-//     });
-//   }
-// }
-
-
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -99,7 +44,8 @@ export class LoginComponent {
       },
       error: (err) => {
         console.log(err);
-        this.errorMessage = err.error?.message || 'Invalid username or password';
+        this.errorMessage =
+          err.error?.message || 'Invalid username or password';
         this.isLoading = false;
       },
     });
