ee292057fefbdd753d664c0c4739f1b5c41371ab
Darren Seah <darrenseahyongliang@protonmail.com>
2025-05-02 00:22:48 +0800

    Price threshold revamp

--- triply-app/src/app/flight-offer/flight-offer.module.ts
+++ triply-app/src/app/flight-offer/flight-offer.module.ts
@@ -8,6 +8,7 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { AuthInterceptor } from '../interceptors/auth.interceptor';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog'; // Import MatDialogModule
 @NgModule({
   declarations: [FlightOfferComponent],
   imports: [
@@ -15,7 +16,8 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: FlightOfferComponent }]),
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule // Add MatDialogModule here
   ],
   providers: [
     provideHttpClient(
