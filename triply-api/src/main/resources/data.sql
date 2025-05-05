7cd5dbebce0977f7bc2fb142efc641ac5e3ca983
b90553110 <b90553110@gmail.com>
2025-05-05 20:26:17 +0800

    Update data.sql

--- triply-api/src/main/resources/data.sql
+++ triply-api/src/main/resources/data.sql
@@ -1707,11 +1707,6 @@ INSERT INTO users (username, password, email, status_id, role_id, created_dt, cr
                                                                                                                       ('System', '$2a$10$giPlj1WXG3RDyLddXUVrY.5AmMYQOIIi0CmFserGhd8eRZx/0ao8e', 'confidentgoldstine1@heywhatsoup.com', 1, 1, 'NOW()', 'SYSTEM', 'NOW()', 'SYSTEM'),
                                                                                                                       ('System2', '$2a$10$giPlj1WXG3RDyLddXUVrY.5AmMYQOIIi0CmFserGhd8eRZx/0ao8e', 'perafe3113@exitings.com', 1, 1, 'NOW()', 'SYSTEM', 'NOW()', 'SYSTEM');
 
-INSERT INTO price_thresholds (user_id, flight_id, hotel_id, threshold_price, created_dt, created_by, updated_dt, updated_by) VALUES
-                                                                                                                                 (1, 1, NULL, 100.00, 'NOW()', 'SYSTEM', 'NOW()', 'SYSTEM'),
-                                                                                                                                 (1, 2, NULL, 150.00, 'NOW()', 'SYSTEM', 'NOW()', 'SYSTEM'),
-                                                                                                                                 (1, 3, NULL, 200.00, 'NOW()', 'SYSTEM', 'NOW()', 'SYSTEM');
-
 INSERT INTO Booking (user_id, final_price, status, booking_time, created_dt, created_by, updated_dt, updated_by) VALUES
                                                                                                                      (1, 200.00, 'Confirmed', '2025-05-01 12:00:00', NOW(), 'SYSTEM', NOW(), 'SYSTEM'),
                                                                                                                      (1, 300.00, 'Confirmed', '2025-05-01 12:00:00', NOW(), 'SYSTEM', NOW(), 'SYSTEM'),
